21831d29b5185096c807f500305db6c864c32038
Serhii Rudenko <svr.phoenix@gmail.com>
2022-12-14 01:17:14 +0100

    Some fixes + slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,30 +1,11 @@
 // SLICK JS для слайдера на сторінці Готель -- доданий С. Руденком
-$('.slider.triple-room__slider').slick({
+$('.slider-2pictures').slick({
   mobileFirst: true,
-  centerMode: true,
-  centerPadding: '0px',
+  // centerMode: true,
+  // centerPadding: '5px',
   slidesToShow: 2,
   adaptiveHeight: true,
-  prevArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button" style="display: block;"><img src="./images/arrow-r.svg" class="prev"/></button>',
+  // prevArrow:
+  //   '<button class="slick-prev slick-arrow" aria-label="Prev" type="button" style="display: block;"><img src="../images/arrow-r.svg" class="prev"/></button>',
   // nextArrow: "<img src='https://svgshare.com/i/6Gf.svg' class='next' alt='2'>",
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        // centerMode: true,
-        // centerPadding: '0px',
-        // slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 414,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 2,
-      },
-    },
-  ],
 });
